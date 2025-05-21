4edb915184e5450af8ccc42332d09984e4c6dc80
Stephane N <stephane.neveu@ssi.gouv.fr>
2025-05-21 08:31:07 +0200

    Autotests: update import path to uapi

--- autotest/src/tests/src-rs/src/test_random.rs
+++ autotest/src/tests/src-rs/src/test_random.rs
@@ -1,43 +1,66 @@
 // SPDX-FileCopyrightText: 2025 ANSSI
 // SPDX-License-Identifier: Apache-2.0
 
-use uapi::exchange::copy_from_kernel;
-use uapi::syscall::*;
+use crate::test_log::*;
+use uapi::status::Status;
 use uapi::systypes::Precision;
-use uapi::systypes::*;
+use uapi::*;
 
-fn test_random_sequence() {
-    for _ in 0..5 {
-        assert_eq!(get_random(), Status::Ok);
-        let mut rng: u32 = 0;
-        copy_from_kernel(&mut rng).unwrap();
-        println!("rng retrieved: 0x{:08x}", rng);
-    }
+pub fn test_random() -> bool {
+    test_suite_start!("sys_get_random");
+    let mut ok = true;
+    ok &= test_random_sequence();
+    ok &= test_random_duration();
+    test_suite_end!("sys_get_random");
+    ok
 }
 
-fn test_random_duration() {
+fn test_random_sequence() -> bool {
+    test_start!();
+    let mut ok = true;
+    let mut rng: u32 = 0;
+    log_info!("get back random value from KRNG");
+    for _ in 0..5 {
+        ok &= check_eq!(__sys_get_random(), Status::Ok);
+        ok &=
+            unsafe { copy_from_kernel(&mut rng as *mut _ as *mut u8, core::mem::size_of::<u32>()) }
+                == Status::Ok;
+        log_info!("rng retrieved: {:#010x}", rng);
+    }
+    test_end!();
+    ok
+}
+
+fn test_random_duration() -> bool {
+    let mut ok = true;
     let mut start: u64 = 0;
     let mut stop: u64 = 0;
     let mut rng: u32 = 0;
+    let mut idx: u32 = 0;
 
-    sched_yield();
-
-    assert_eq!(get_cycle(Precision::Microseconds), Status::Ok);
-    copy_from_kernel(&mut start).unwrap();
+    ok &= check_eq!(__sys_sched_yield(), Status::Ok);
+    ok &= check_eq!(__sys_get_cycle(Precision::Microseconds), Status::Ok);
+    ok &= unsafe { copy_from_kernel(&mut start as *mut _ as *mut u8, core::mem::size_of::<u64>()) }
+        == Status::Ok;
 
     for _ in 0..=1000 {
-        assert_eq!(get_random(), Status::Ok);
-        copy_from_kernel(&mut rng).unwrap();
+        ok &= check_eq!(__sys_get_random(), Status::Ok);
+        ok &=
+            unsafe { copy_from_kernel(&mut rng as *mut _ as *mut u8, core::mem::size_of::<u32>()) }
+                == Status::Ok;
+        idx += 1;
     }
 
-    assert_eq!(get_cycle(Precision::Microseconds), Status::Ok);
-    copy_from_kernel(&mut stop).unwrap();
+    ok &= check_eq!(__sys_get_cycle(Precision::Microseconds), Status::Ok);
+    ok &= unsafe { copy_from_kernel(&mut stop as *mut _ as *mut u8, core::mem::size_of::<u64>()) }
+        == Status::Ok;
 
-    let avg_duration = ((stop - start) / 1000) as u32;
-    println!("average get_random+copy cost: {}", avg_duration);
-}
+    if idx > 0 {
+        log_info!(
+            "average get_random+copy cost: {} µs",
+            ((stop - start) / idx as u64) as u32
+        );
+    }
 
-pub fn test_random() {
-    test_random_sequence();
-    test_random_duration();
+    ok
 }
