b17cd856c651893975aaad5b42f2cf52d286532d
Stephane N <stephane.neveu@ssi.gouv.fr>
2025-05-22 14:43:30 +0200

    Autotests: SHMs get shm names from dts

--- autotest/src/tests/src-rs/src/devices_utils.rs
+++ autotest/src/tests/src-rs/src/devices_utils.rs
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2025 ANSSI
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::devices::{DEVICE_NAMES, DEVICES};
+use crate::devices::{DEVICE_NAMES, DEVICES, SHMS, SHM_NAMES};
 use uapi::systypes::DevInfo;
 
 /// Search for a device by its name
@@ -13,4 +13,11 @@ pub fn get_device_by_name(name: &str) -> Option<&'static DevInfo> {
 /// Iterate over all devices
 pub fn iter_devices() -> impl Iterator<Item = (&'static str, &'static DevInfo)> {
     DEVICE_NAMES.iter().copied().zip(DEVICES.iter())
+}
+
+/// Get SHM by name
+pub fn get_shm_by_name(name: &str) -> Option<&'static ShmInfo> {
+    SHM_NAMES.iter()
+        .position(|&n| n == name)
+        .map(|i| &SHMS[i])
 }
